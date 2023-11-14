683039969975d1a1358d648eb89a853489e76a6b
chahine-mahfoudh <chahine.mahfoudh@esprit.tn>
2023-11-14 01:54:42 +0000

    Deploy to Nexus

---
+++ src/main/java/tn/esprit/devops_project/entities/ActivitySectorDTO.java
@@ -0,0 +1,30 @@
+package tn.esprit.devops_project.entities;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ActivitySectorDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long idSecteurActivite;
+    private String codeSecteurActivite;
+    private String libelleSecteurActivite;
+
+
+    // Constructors, getters, setters...
+
+}
